d2b8497265a2ba5f3edfe2fbb1c906ebeed80282
mwilliams113 <mwilliams@clinicalink.com>
2020-08-18 18:52:42 -0400

    adding missing files

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,38 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/chrome-icon.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+//This causes an exception and must be commented out for the welcome message and button to do anything.
+//let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
